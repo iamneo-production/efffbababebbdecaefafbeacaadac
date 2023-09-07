4cb4c825bfb330eac21385e3be6a3e7a0f584541
Iamneo <techadmin@iamneo.ai>
2023-09-07 07:33:17 +0000

    Backup Commit

--- sample/sam.java
+++ sample/sam.java
@@ -3,7 +3,10 @@ public class sam
 {
     public static void main(String[] args) {
         for (int index = 0; index <11; index++) {
-            System.out.print();
+            for (int i = 0; i < index; i++) {
+                System.out.print("*");               
+            }
+            System.out.println();
         }   
     }
 }
